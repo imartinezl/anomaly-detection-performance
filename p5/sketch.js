bbc2a7b9a11c969613ffc65ee8acce6f66a96ba9
imartinezl <inigomlap@gmail.com>
2019-12-26 17:13:08 +0100

    Added axis lines and rug plot on margin

--- p5/sketch.js
+++ p5/sketch.js
@@ -18,11 +18,14 @@ setup = () => {
     f_x.push(pdf(value, 0, 1.5));
     s_x.push(pdf(value, 0, 1));
   }
+
+  // get ordered indexes
   indices = new Array(n_x);
   for (var i = 0; i < n_x; ++i) indices[i] = i;
   indices.sort(function (a, b) { return x[a] < x[b] ? -1 : x[a] > x[b] ? 1 : 0; });
 
   // generate uniform data
+  lim_y = Math.max(Math.max.apply(Math, f_x), Math.max.apply(Math, s_x));
   lim_inf = Math.min.apply(Math, x);
   lim_sup = Math.max.apply(Math, x);
   volume_support = lim_sup - lim_inf;
@@ -36,13 +39,40 @@ setup = () => {
   // generate alpha
 
   translate(width/2, height/2);
+
+  let sx = 50, sy=500;
+
+  line(lim_inf*sx, 0, lim_sup*sx, 0);
+  line(0, 0, 0, -lim_y*sy);
+
   noFill();
+  stroke(255,0,0);
   beginShape();
   for (let i = 0; i < n_x; i++) {
     let pos = indices[i]
-    vertex(x[pos]*30, -f_x[pos]*300);
+    vertex(x[pos]*sx, -f_x[pos]*sy);
   }
   endShape();
+
+  noFill();
+  stroke(0,255,0);
+  beginShape();
+  for (let i = 0; i < n_x; i++) {
+    let pos = indices[i]
+    vertex(x[pos]*sx, -s_x[pos]*sy);
+  }
+  endShape();
+
+  noFill();
+  stroke(0, 50);
+  strokeWeight(0.5);
+  for (let i = 0; i < n_x; i++) {
+    push();
+    let pos = indices[i]
+    translate(x[pos]*sx, 10);
+    line(0,0,0,10);
+    pop();
+  }
 
 
   console.log();
