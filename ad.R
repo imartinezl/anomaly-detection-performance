6ffe27b5cdd6ffc6a3b41b1033c3e90614bc7ceb
imartinezl <inigomlap@gmail.com>
2019-12-26 15:39:48 +0100

    Refactor into functions

--- ad.R
+++ ad.R
@@ -1,23 +1,22 @@
 library(dplyr)
 
-
-
 # AREA-UNDER-CURVE --------------------------------------------------------
 auc <- function(x,y){
   return(sum((x-lag(x))*(y+lead(y))/2,na.rm=T))
 }
 
 # MASS-VOLUME -------------------------------------------------------------
-mv <- function(axis_alpha, volume_support, s_unif, s_X, n_generated){
+mass_volume <- function(s_X, s_unif, alpha, volume_support){
+  n_generated <- length(s_unif)
   n <- length(s_X)
   s_X_order <- order(s_X)
   mass <- 0
   cpt <- 0
   u <- s_X[s_X_order[n-cpt]]
-  mv <- numeric(length(axis_alpha))
-  threshold <- numeric(length(axis_alpha))
-  for(i in 1:length(axis_alpha)){
-    while(mass < axis_alpha[i]){
+  mv <- numeric(length(alpha))
+  threshold <- numeric(length(alpha))
+  for(i in 1:length(alpha)){
+    while(mass < alpha[i]){
       cpt <- cpt + 1
       u <- s_X[s_X_order[n-cpt]]
       mass <- cpt / n
@@ -25,53 +24,13 @@ mv <- function(axis_alpha, volume_support, s_unif, s_X, n_generated){
     threshold[i] <- u
     mv[i] <- sum(s_unif >= u) / n_generated * volume_support
   }
-  return(list(auc_mv = auc(axis_alpha, mv), mv = mv, threshold=threshold))
+  return(list(auc_mv = auc(alpha, mv), mv = mv, threshold=threshold))
 }
 
-i <- 90
-alpha <- axis_alpha[i]
-u <- mv_result$threshold[i]
-mass <- seq(0,1,length.out = length(s_X))
-data.frame(mass, s_X, alpha, u) %>% 
-  dplyr::mutate(s_X_sorted = sort(s_X)) %>% 
-  ggplot2::ggplot()+
-  ggplot2::geom_line(ggplot2::aes(x=mass, y=s_X_sorted), size=2)+
-  ggplot2::geom_point(ggplot2::aes(x=1-alpha,y=u), color="red")+
-  ggplot2::geom_vline(ggplot2::aes(xintercept=1-alpha), linetype="dashed")+
-  ggplot2::geom_hline(ggplot2::aes(yintercept=u), linetype="dashed")
-
-
-dif <- s_unif[order(unif)]-u
-xp_index <- which(diff(sign(dif)) != 0)
-xp_index <- c(1,xp_index,n_generated-1)
-xp_sign <- sign(dif)[xp_index]
-xp <- sort(unif)[xp_index]
-cut_points <- data.frame(xp, xp_sign) %>% 
-  dplyr::arrange(xp) %>% 
-  dplyr::mutate(xp_next = lead(xp),
-                xp_sign_next = lead(xp_sign),
-                good = xp_sign*xp_sign_next == -1) %>% 
-  dplyr::filter(good) %>% 
-  dplyr::slice(seq(1,n(),2)) %>%
-  dplyr::select(xp, xp_next) %>% 
-  dplyr::mutate(p = 1:n())
-cut_points
-
-data.frame(unif, s_unif, u) %>% 
-  merge(cut_points) %>% 
-  dplyr::mutate(f = unif >= xp & unif <= xp_next) %>% 
-  ggplot2::ggplot()+
-  ggplot2::geom_area(data=. %>% dplyr::filter(f), ggplot2::aes(x=unif, y=s_unif, group=p), fill="red")+
-  # ggplot2::geom_ribbon(data=. %>% filter(f), ggplot2::aes(x=unif, ymin=u, ymax=s_unif), fill="red")
-  ggplot2::geom_line(data=. %>% dplyr::filter(p==1), ggplot2::aes(x=unif, y=s_unif), size=2)+
-  ggplot2::geom_hline(yintercept=u, linetype="dashed")+
-  ggplot2::geom_segment(data=cut_points, ggplot2::aes(x=xp, y=-0.1, xend=xp_next, yend=-0.1),
-                        arrow = ggplot2::arrow(length = grid::unit(10, 'pt'), type = "closed", angle=15, ends = "both"))
-
-
 # EXCESS-MASS -------------------------------------------------------------
-em <- function(t, t_max, volume_support, s_unif, s_X, n_generated){
+excess_mass <- function(s_X, s_unif, t, t_max, volume_support){
   EM_t <- numeric(length(t))
+  n_generated <- length(s_unif)
   n <- length(s_X)
   s_X_unique <- unique(s_X)
   EM_t[1] <- 1.0
@@ -100,11 +59,13 @@ scoring <- function(x){
   return(dnorm(x,0,1.5)+dnorm(x,10,1.2))
 }
 
+# Generate data
 n <- 1000
 X <- generator(n)
 f_X <- true_density(X)
 s_X <- scoring(X)
 
+# Generate uniform data
 lim_inf <- min(X)
 lim_sup <- max(X)
 volume_support <- prod(lim_sup - lim_inf)
@@ -112,42 +73,108 @@ n_generated <- 10000
 unif <- runif(n_generated, lim_inf, lim_sup)
 f_unif <- true_density(unif)
 s_unif <- scoring(unif)
-alpha_min <- 0.9
+
+# Generate alpha
+alpha_min <- 0
 alpha_max <- 0.999
-axis_alpha <- seq(alpha_min, alpha_max, 0.0001)
+alpha <- seq(alpha_min, alpha_max, 0.0001)
 
-mv_result <- mv(axis_alpha, volume_support, s_unif, s_X, n_generated)
-plot(axis_alpha, mv_result$mv)
+# MASS-VOLUME
+mv_result <- mass_volume(s_X, s_unif, alpha, volume_support)
 
-data.frame(alpha=axis_alpha, mv=mv_result$mv, auc_mv=mv_result$auc_mv) %>% 
-  ggplot2::ggplot()+
-  ggplot2::geom_line(ggplot2::aes(x=alpha, y=mv), size=2)+
-  ggplot2::geom_text(ggplot2::aes(x=alpha[1], y=mv[1], label=round(auc_mv,2)), check_overlap = T)
+mv <- mv_result$mv
+auc_mv <- mv_result$auc_mv
+t_mv <- mv_result$threshold
 
-# data.frame(x, s_X, f_X) %>%
-  # tidyr::gather(key,value, s_X, f_X) %>% 
-data.frame(X=unif, Scoring=s_unif, True_Density=f_unif) %>%
-  tidyr::gather(key,value, -X) %>% 
-  ggplot2::ggplot()+
-  ggplot2::geom_line(ggplot2::aes(x=X, y=value, color=key), size=2)+
-  ggplot2::geom_hline(yintercept = 0)+
-  ggplot2::geom_vline(xintercept = lim_inf, linetype="dashed")+
-  ggplot2::geom_vline(xintercept = lim_sup, linetype="dashed")+
-  ggplot2::geom_segment(ggplot2::aes(x=lim_inf, y=-0.05, xend=lim_sup, yend=-0.05), alpha=0.01,
-                        arrow = ggplot2::arrow(length = grid::unit(10, 'pt'), type = "closed", angle=15, ends = "both"))+
-  ggplot2::geom_label(x=(lim_inf+lim_sup)/2, y=-0.05, label="Volume support", check_overlap = T)+
-  ggplot2::scale_color_manual(name=NULL, values=c("red","blue"))+
-  ggplot2::labs(x="X",y="Density")+
-  # ggplot2::ylim(c(-1,NA))+
-  # ggplot2::theme_minimal(base_family = "Roboto Condensed")+
-  hrbrthemes::theme_ipsum_rc()+
-  ggplot2::theme(legend.position = "top")
+# PLOT FUNCTIONS ----------------------------------------------------------
 
-
-
+plot_mv <- function(alpha, mv, auc_mv, i){
+  data.frame(alpha=alpha, mv=mv, auc_mv=auc_mv) %>% 
+    ggplot2::ggplot()+
+    ggplot2::geom_line(ggplot2::aes(x=alpha, y=mv), size=2)+
+    ggplot2::geom_area(ggplot2::aes(x=alpha, y=mv), fill="#F6F6F6")+
+    ggplot2::geom_text(ggplot2::aes(x=mean(alpha), y=mv[1]/2, label=round(auc_mv,3)), check_overlap = T)+
+    ggplot2::geom_segment(x=alpha[i], xend=alpha[i], y=0, yend=mv[i])+
+    ggplot2::geom_segment(x=min(alpha), xend=alpha[i], y=mv[i], yend=mv[i])+
+    ggplot2::geom_text(x=alpha[i], y=0, label=alpha[i], vjust=2, check_overlap = T)+
+    ggplot2::geom_text(x=min(alpha), y=mv[i], label=round(mv[i],1), hjust=0, vjust=-0.5, check_overlap = T)+
+    ggplot2::labs(x=expression(alpha), y="Volume")+
+    hrbrthemes::theme_ipsum_rc()+
+    ggplot2::theme(
+      axis.title.x = ggplot2::element_text(size=12),
+      axis.title.y = ggplot2::element_text(size=12)
+    )
+}
+plot_density <- function(X, s_X, f_X){
+  lim_inf <- min(X)
+  lim_sup <- max(X)
   
+  data.frame(X=X, Scoring=s_X, True_Density=f_X) %>%
+    tidyr::gather(key,value, -X) %>% 
+    ggplot2::ggplot()+
+    ggplot2::geom_line(ggplot2::aes(x=X, y=value, color=key), size=1)+
+    ggplot2::geom_hline(yintercept = 0)+
+    ggplot2::geom_vline(xintercept = lim_inf, linetype="dashed")+
+    ggplot2::geom_vline(xintercept = lim_sup, linetype="dashed")+
+    ggplot2::geom_segment(ggplot2::aes(x=lim_inf, y=-0.05, xend=lim_sup, yend=-0.05), alpha=0.01,
+                          arrow = ggplot2::arrow(length = grid::unit(10, 'pt'), type = "closed", angle=15, ends = "both"))+
+    ggplot2::geom_text(x=(lim_inf+lim_sup)/2, y=-0.05, label="Volume support", check_overlap = T)+
+    ggplot2::scale_color_manual(name=NULL, values=c("red","blue"))+
+    ggplot2::labs(x="X",y="Density")+
+    hrbrthemes::theme_ipsum_rc()+
+    ggplot2::theme(legend.position = "top")
+}
+plot_scoring <- function(s_X, alpha, t_mv, i){
+  data.frame(s_X) %>% 
+    dplyr::mutate(s_X_sorted = sort(s_X),
+                  mass = seq(0,1,length.out = n())) %>% 
+    ggplot2::ggplot()+
+    ggplot2::geom_line(ggplot2::aes(x=1-mass, y=s_X_sorted), size=2)+
+    ggplot2::geom_point(ggplot2::aes(x=alpha[i],y=t_mv[i]), color="red")+
+    ggplot2::geom_vline(ggplot2::aes(xintercept=alpha[i]), linetype="dashed")+
+    ggplot2::geom_hline(ggplot2::aes(yintercept=t_mv[i]), linetype="dashed")
+}
+plot_volume <- function(unif, s_unif, alpha, t_mv, i){
+  n_generated <- length(unif)
+  dif <- s_unif[order(unif)]-t_mv[i]
+  xp_index <- which(diff(sign(dif)) != 0)
+  xp_index <- c(1,xp_index,n_generated-1)
+  xp_sign <- sign(dif)[xp_index]
+  xp <- sort(unif)[xp_index]
+  cut_points <- data.frame(xp, xp_sign) %>% 
+    dplyr::arrange(xp) %>% 
+    dplyr::mutate(xp_next = lead(xp),
+                  xp_sign_next = lead(xp_sign),
+                  good = xp_sign*xp_sign_next == -1) %>% 
+    dplyr::filter(good) %>% 
+    dplyr::slice(seq(1,n(),2)) %>%
+    dplyr::select(xp, xp_next) %>% 
+    dplyr::mutate(p = 1:n(),
+                  d = xp_next - xp)
+  cut_points
+  cut_points$d %>% sum
+  data.frame(unif, s_unif, t_mv[i]) %>% 
+    merge(cut_points) %>% 
+    dplyr::mutate(f = unif >= xp & unif <= xp_next) %>% 
+    ggplot2::ggplot()+
+    ggplot2::geom_area(data=. %>% dplyr::filter(f), ggplot2::aes(x=unif, y=s_unif, group=p), fill="red")+
+    # ggplot2::geom_ribbon(data=. %>% filter(f), ggplot2::aes(x=unif, ymin=u, ymax=s_unif), fill="red")
+    ggplot2::geom_line(data=. %>% dplyr::filter(p==1), ggplot2::aes(x=unif, y=s_unif), size=2)+
+    ggplot2::geom_hline(yintercept=t_mv[i], linetype="dashed")+
+    ggplot2::geom_segment(data=cut_points, ggplot2::aes(x=xp, y=-0.05, xend=xp_next, yend=-0.05),
+                          arrow = ggplot2::arrow(length = grid::unit(10, 'pt'), type = "closed", angle=15, ends = "both"))
+}
+
+plot_mv(alpha, mv, auc_mv, 900)
+plot_density(X, s_X, f_X)
+plot_density(unif, s_unif, f_unif)
+plot_scoring(s_X, alpha, t_mv, i)
+
+plot_volume(unif, s_unif, alpha, t_mv, i) %>% ggplot2::ggsave(filename = paste0("Volume_",i,".png"), device = "png", width = 9, height = 6)
+
+# EXCESS - MASS 
 t <- seq(0, 100 / volume_support, by=0.01 / volume_support)
 t_max <- 0.9
-em_result <- em(t, t_max, volume_support, s_unif, s_X, n_generated)
+em_result <- em(s_X, s_unif, t, t_max, volume_support)
 plot(t, em_result$EM_t)
 plot(t[1:em_result$amax], em_result$EM_t[1:em_result$amax])
