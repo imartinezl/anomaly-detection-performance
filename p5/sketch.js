7943729aeea75a214283b8ea319f77cada088edf
imartinezl <inigomlap@gmail.com>
2019-12-27 11:35:27 +0100

    Calculated cut_points (to be refactored)

--- p5/sketch.js
+++ p5/sketch.js
@@ -20,9 +20,9 @@ setup = () => {
   }
 
   // get ordered indexes
-  i_x = new Array(n_x);
-  for (var i = 0; i < n_x; ++i) i_x[i] = i;
-  i_x.sort(function (a, b) { return x[a] < x[b] ? -1 : x[a] > x[b] ? 1 : 0; });
+  id_x = new Array(n_x);
+  for (var i = 0; i < n_x; ++i) id_x[i] = i;
+  id_x.sort(function (a, b) { return x[a] < x[b] ? -1 : x[a] > x[b] ? 1 : 0; });
 
   // generate uniform data
   lim_y = Math.max(Math.max.apply(Math, f_x), Math.max.apply(Math, s_x));
@@ -49,18 +49,18 @@ setup = () => {
 
   translate(width/2, height/2);
 
-  let sx = 50, sy=500;
+  let sx = 50, sy=-500;
 
   line(lim_inf*sx, 0, lim_sup*sx, 0);
-  line(0, 0, 0, -lim_y*sy);
+  line(0, 0, 0, lim_y*sy);
 
   // plot true density
   noFill();
   stroke(255,0,0);
   beginShape();
   for (let i = 0; i < n_x; i++) {
-    let pos = i_x[i]
-    vertex(x[pos]*sx, -f_x[pos]*sy);
+    let pos = id_x[i]
+    vertex(x[pos]*sx, f_x[pos]*sy);
   }
   endShape();
 
@@ -69,8 +69,8 @@ setup = () => {
   stroke(0,255,0);
   beginShape();
   for (let i = 0; i < n_x; i++) {
-    let pos = i_x[i]
-    vertex(x[pos]*sx, -s_x[pos]*sy);
+    let pos = id_x[i]
+    vertex(x[pos]*sx, s_x[pos]*sy);
   }
   endShape();
 
@@ -80,32 +80,70 @@ setup = () => {
   strokeWeight(0.5);
   for (let i = 0; i < n_x; i++) {
     push();
-    let pos = i_x[i]
+    let pos = id_x[i]
     translate(x[pos]*sx, 10);
     line(0,0,0,10);
     pop();
   }
 
   // calculate mass-volume
-  i_unif = new Array(n_unif);
-  for (var i = 0; i < n_unif; ++i) i_unif[i] = i;
-  i_unif.sort(function (a, b) { return unif[a] < unif[b] ? -1 : unif[a] > unif[b] ? 1 : 0; });
+  id_unif = new Array(n_unif);
+  for (var i = 0; i < n_unif; ++i) id_unif[i] = i;
+  id_unif.sort(function (a, b) { return unif[a] < unif[b] ? -1 : unif[a] > unif[b] ? 1 : 0; });
 
-  let t = 0.3;
+  let t = 0.1;
+  line(lim_inf*sx,t*sy,lim_sup*sx,t*sy);
   let dif = [], dif_sign = [];
   for (let i = 0; i < n_unif; i++) {
-    let pos = i_unif[i];
+    let pos = id_unif[i];
     dif.push(s_unif[pos]-t);
-    dif_sign.push(Math.sign(dif));
+    dif_sign.push(Math.sign(s_unif[pos]-t));
   }
-  console.log(dif);
+  //console.log(dif, dif_sign);
+  let xp_index = [0];
+  let xp_sign = [dif_sign[0]];
   for (let i = 0; i < n_unif-1; i++) {
-    let d = dif[i+1]-dif[i]; // TO-DO
-    if(d === 0){
-      console.log(i);
+    let d = dif_sign[i+1]-dif_sign[i];
+    if(d !== 0){
+      //console.log(dif_sign[i], dif_sign[i+1]);
+      //console.log(i,id_unif[i],unif[id_unif[i]],s_unif[id_unif[i]])
+      xp_index.push(i);
+      xp_sign.push(dif_sign[i]);
     }
   }
+  xp_index.push(n_unif-1);
+  xp_sign.push(dif_sign[n_unif-2])
+  console.log(xp_index, xp_sign);
+  let cont = 0;
+  let cut_points = [];
+  for (let i = 0; i < xp_index.length-1; i++) {
+    let good = xp_sign[i+1]*xp_sign[i];
+    console.log(good, xp_index[i], xp_index[i+1], unif[id_unif[xp_index[i]]], unif[id_unif[xp_index[i+1]]]);
+    if(good === -1){
+      if(cont % 2 === 0){
+        cut_points.push({id_xp:xp_index[i],id_xp_next:xp_index[i+1]});
+      }
+      cont++;
+    }
+    
+  }
+  console.log(cut_points);
+  for (let i = 0; i < cut_points.length; i++) {
+    const e = cut_points[i];
+    console.log(e.id_xp, e.id_xp_next);
+    strokeWeight(4);
+    //line(unif[id_unif[e.id_xp]]*sx, t*sy, unif[id_unif[e.id_xp_next]]*sx, t*sy);
+    beginShape();
+    fill(255, 0, 0, 100);
+    //vertex(unif[id_unif[e.id_xp]]*sx,0*sy);
+    for(let j=e.id_xp; j <= e.id_xp_next; j++){
+      vertex(unif[id_unif[j]]*sx, s_unif[id_unif[j]]*sy);
+    }
+    //vertex(unif[id_unif[e.id_xp_next]]*sx,0*sy);
+    endShape(CLOSE);
 
+    
+  }
 
   console.log();
 }
