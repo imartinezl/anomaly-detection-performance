c795028aa169f7cf9a9fc51f11a9485f5d3c7a03
imartinezl <inigomlap@gmail.com>
2019-12-29 19:00:53 +0100

    CopyWebpackPlugin

--- mass-volume-viz/webpack.config.js
+++ mass-volume-viz/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -21,6 +22,12 @@ module.exports = {
             title: 'Output Management',
             template: './index.html'
         }),
+        new CopyWebpackPlugin([{
+            from: 'assets',
+            to: 'assets'
+        }], {
+            copyUnmodified: true
+        })
     ],
     module: {
         rules: [{
