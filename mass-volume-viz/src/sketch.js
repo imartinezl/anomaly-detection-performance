89d24030189090a57ea2f916cd5165922350b3c0
imartinezl <inigomlap@gmail.com>
2019-12-29 19:01:49 +0100

    Sketch function

--- mass-volume-viz/src/sketch.js
+++ mass-volume-viz/src/sketch.js
@@ -1,28 +1,90 @@
-import p5 from './p5.min.js';
-global.p5 = p5;
+import './setup.js';
+import Distribution from './distribution.js';
 
-export default class Sketch {
+let fontRegular, fontSemiBold, fontBold;
+let sketch = (p5) => {
 
-    constructor() {
-        new p5(this.sketch);
+    var j, jj, alpha, distributions;
+
+    let init = () => {
+        j = 0;
+        jj = 1;
+        alpha = generate_alpha();
+        distributions = generate_distributions(alpha);
+        console.log('init');
     }
 
-    sketch = (p5) => {
 
-        p5.preload = () => {
-            // fontRegular = loadFont('assets/Kulim_Park/KulimPark-Regular.ttf');
-            // fontSemiBold = loadFont('assets/Kulim_Park/KulimPark-SemiBold.ttf');
-            // fontBold = loadFont('assets/Kulim_Park/KulimPark-Bold.ttf');
+    p5.preload = () => {
+        fontRegular = p5.loadFont('./assets/Kulim_Park/KulimPark-Regular.ttf');
+        // fontSemiBold = loadFont('assets/Kulim_Park/KulimPark-SemiBold.ttf');
+        // fontBold = loadFont('assets/Kulim_Park/KulimPark-Bold.ttf');
+    }
+
+
+
+    p5.setup = () => {
+        var canvas = p5.createCanvas(1000, 1000);
+        canvas.parent('sketch-holder');
+        p5.background("#F6F6F6");
+        init();
+    }
+
+    p5.draw = () => {
+        p5.background("#F6F6F6");
+        p5.circle(p5.mouseX, p5.mouseY, 10);
+        for (let i = 0; i < distributions.length; i++) {
+            distributions[i].display(j);
         }
-
-        p5.setup = () => {
-            var canvas = p5.createCanvas(100, 100);
-            canvas.parent('sketch-holder');
-            p5.background(255, 0, 200);
+        j += jj;
+        if (j >= alpha.length - 1 | j < 0) {
+            jj *= -1;
+            j += jj;
         }
     }
 
+    let generate_alpha = () => {
+        //generate alpha
+        let alpha_min = 0; //alpha_min_slider.value();
+        let alpha_max = 0.99;
+        let alpha_by = 0.005;
+        let alpha = [];
+        for (let a = alpha_min; a <= alpha_max; a += alpha_by) {
+            alpha.push(a);
+        }
+        return alpha;
+    }
+
+
+    let generate_distributions = (alpha) => {
+        let n_x = 1000;
+        let n_unif = 10000;
+        let posA = p5.createVector(300, 400);
+        let sclA = p5.createVector(70, -700);
+        let posB = p5.createVector(600, 400);
+        let sclB = p5.createVector(300, -50);
+
+        let gen = { f: generateGaussian, p: [0, 1] };
+        let pdf = { f: dnorm, p: [0, 1] };
+        //gen = { f: custom_gen, p: [] };
+        //pdf = { f: custom_pdf, p: [] };
+        let dA = new Distribution(p5, n_x, n_unif, gen, pdf, alpha, posA, sclA, posB, sclB);
+
+        // posA = createVector(300, 800);
+        // sclA = createVector(70, -700);
+        // posB = createVector(600, 800);
+        // sclB = createVector(300, -50);
+        // gen = { f: randomGaussian, p: [0, 1] };
+        // pdf = { f: dnorm, p: [0, 1.2] };
+        // let dB = new Distribution(n_x, n_unif, gen, pdf, alpha, posA, sclA, posB, sclB);
+
+        // distributions = [dA, dB];
+        return [dA];
+        // return distributions;
+    }
 }
+
+export default sketch;
 
 
 
